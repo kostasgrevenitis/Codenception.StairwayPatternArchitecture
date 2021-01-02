cd91a37667a54dfdb46b96cfdb876f8c3b16e455
Konstantinos Grevenitis <konstantinos.grevenitis@outlook.com>
2021-01-02 17:00:18 +0200

    client added

---
+++ Codenception.StairwayPatternArchitecture.Domain.Clients.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codenception.StairwayPatternArchitecture.Domain.Clients.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Codenception.StairwayPatternArchitecture.Mappers/RestaurantMapper.cs
@@ -0,0 +1,19 @@
+﻿using Codenception.StairwayPatternArchitecture.Domain.Interfaces.Restaurant.Mappers;
+using Codenception.StairwayPatternArchitecture.Domain.Interfaces.Restaurant.Records;
+using System;
+
+namespace Codenception.StairwayPatternArchitecture.Mappers
+{
+    public class RestaurantMapper : IMapper<IDomainRecord, IDatabaseRecord<System.ValueType>>
+    {
+        public IDatabaseRecord<ValueType> MapToDatabaseRecord(IDomainRecord databaseRecord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDomainRecord MapToRecord(IDatabaseRecord<ValueType> databaseRecord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
